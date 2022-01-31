35a833d8b3c7fa5b37cded3688ae8fa0db32d24f
Alex J. Luna <alejolumend@gmail.com>
2022-01-31 15:25:22 +0100

    se agrega plugin Daft

--- daft-punk-lunawp-show.php
+++ daft-punk-lunawp-show.php
@@ -7,30 +7,34 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-CURLOPT_URL => "https://api.spotify.com/v1/artists/4tZwfgrHOc3mvqYlEYSvVi/albums?limit=50",
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_ENCODING => "",
-CURLOPT_MAXREDIRS => 10,
-CURLOPT_TIMEOUT => 0,
-CURLOPT_FOLLOWLOCATION => true,
-CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-CURLOPT_CUSTOMREQUEST => "GET",
-CURLOPT_HTTPHEADER => array(
-"Authorization: Bearer BQB34Yh_dn_TY6vgUW-aL1yDFt1anuw3i0JYM9Qei90SZLx5xkObpHtbgr8XFixD05vwxCtHNZxK3Jtm2bP2KM1aDuunh0xCthBc-qbbwO3rwRFr7Ss-RiRRxUfmx9GSrjvHjPiR8p9DnFT3x_yBrx2nLdWgwjb_mF4"
+  CURLOPT_URL => 'https://api.spotify.com/v1/artists/4tZwfgrHOc3mvqYlEYSvVi/albums',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'Authorization: Bearer BQDEygsqLGmqPWrHPDs10V9o1kvODQDFn6-ok1QY6MwopNszHxyQ5p10vZNEeQqFxq8C0bKRZzAIeGJOh6TSSlNXB4RowZ7E-dkacNWxPdmf1oGdYh9IpVJASGha6lfqu7p3IGbC82vGTu2y0ZIidll8uSUE6pe7zU4'
   ),
- ));
+));
 
 $response = curl_exec($curl);
-$data = json_decode($response, true); 
 
+curl_close($curl);
+
+$data = json_decode( $response, true );
+
+if( !isset( $data['items'] ) || !is_array( $data['items'] ) )
+  return;
 /**
  * Mostrando Datos
  */
 echo "<h3>Albumes Daft Punk</h3><br>";
   foreach($data as $valor){
     for($i=0; $i<count($data['items']); $i++){
-        echo "<strong>".$data['items'][$i]['name']."</strong>, Fecha de Publicación: ".$data['items'][$i]['release_date']."<br>";}
-   exit;
+        echo "<strong>".$data['items'][$i]['name']."</strong>, Fecha de Publicación: ".date( "d/m/Y", strtotime( $data['items'][$i]['release_date'] ) )."<br>";
     }
-            
+  }  
     
--- daft-punk-lunawp.php
+++ daft-punk-lunawp.php
@@ -14,30 +14,69 @@
  */
 
 if(!defined('ABSPATH'))
-exit();
+    exit();
 
 //constantes
 define('DAFTPUNKLUNWP_NAME','Daft Punk LunaWP');
+define('DAFTPUNKLUNWP_PREFIX','daft_punk_lunawp_');
 define('DAFTPUNKLUNWP_PATH', plugin_dir_path(__FILE__));
 define('DAFTPUNKLUNWP_ADMIN_PATH', plugin_dir_path(__FILE__).'/admin/');
 
+function daft_punk_lunawp_activate(){
+    $opciones = array(
+        'id_singer' => '',
+    );
+
+    foreach( $opciones as $key => $value )
+        add_option( DAFTPUNKLUNWP_PREFIX . $key, $value, '', false );
+}
+register_activation_hook( __FILE__, ' daft_punk_lunawp_activate' );
+
+function daft_punk_lunawp_deactivate(){
+}
+register_deactivation_hook( __FILE__, ' daft_punk_lunawp_deactivate' );
+
 //menu admin
-function daft_punk_lunawp_menu()
-{
-    add_menu_page(DAFTPUNKLUNWP_NAME, DAFTPUNKLUNWP_NAME, 'manage_options', DAFTPUNKLUNWP_ADMIN_PATH.'idSinger.php');
+function daft_punk_lunawp_menu(){
+    add_menu_page(DAFTPUNKLUNWP_NAME, DAFTPUNKLUNWP_NAME, 'manage_options', 'daft_punk_lunawp_admin', 'daft_punk_lunawp_admin_gui');
 }
 add_action('admin_menu', 'daft_punk_lunawp_menu');
 
-//funcion para registrar ID Artista
-function daft_punk_lunawp_setting()
-{
-    register_setting('daft-punk-lunawp-setting-id', 'daft_punk_lunawp_id_singer');
+function daft_punk_lunawp_admin_gui(){
+    if( isset( $_POST ) && !empty( $_POST ) ){
+        if( !current_user_can( 'manage_options' ) )
+            wp_die();
+
+        update_option( 'daft_punk_lunawp_id_singer', sanitize_text_field( $_POST['daft_punk_lunawp_id_singer'] ), false );
+    }
+    ?>
+    <div class='wrap'>
+        <h2><?php _e('Daft Punk Albums', 'daft-punk-wp'); ?></h2>
+        <p>Bienvenidos a la configuracion</p>
+
+        <form action="" method="POST">
+             <table class="form-table">
+                <tr>
+                    <td>
+                        <label><?php _e('Introduce ID del Artista', 'daft-punk-wp'); ?>:</label>
+                        <input type="text" name="daft_punk_lunawp_id_singer" id="daft_punk_lunawp_id_singer" value="<?php echo get_option('daft_punk_lunawp_id_singer'); ?>" />
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+    </div>
+    <?php
 }
-add_action('admin_init', 'daft_punk_lunawp_setting');
 
 //short code [daft_punk_sc] para mostrar los datos en cualquier pagina
-function show_daft_punk_lunawp()
-{
-    return $options = wp_remote_retrieve_body( wp_remote_get( plugins_url() . '/daft-punk-lunawp/daft-punk-lunawp-show.php' ) );
+function show_daft_punk_lunawp(){
+    ob_start();
+    
+    include( DAFTPUNKLUNWP_PATH . "daft-punk-lunawp-show.php" );
+
+    $output_string = ob_get_contents();
+	ob_end_clean();
+	return $output_string;
 }
-add_shortcode('daft_punk_sc', 'show_daft_punk_lunawp');
+add_shortcode( 'daft_punk_sc', 'show_daft_punk_lunawp' );
